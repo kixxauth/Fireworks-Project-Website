6b7f10a60cc24993dd633e4519b8d6f0fb8f83c0
Kristoffer Walker <kixxauth@gmail.com>
2009-11-22 11:44:59 -0500

    added content mngr and templating functionality

--- google-app-engine/request.py
+++ google-app-engine/request.py
@@ -5,6 +5,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
 from django.utils import simplejson
+from google.appengine.ext.webapp import template
 
 import os
 import re
@@ -32,6 +33,8 @@ def sanitizePageName(name):
   return 'page:'+ name
 
 def desanitizePageName(name):
+  if not name:
+    return name
   return name.replace('page:', '')
 
 def getRenderedPage(name):
@@ -79,7 +82,7 @@ class NotFoundHandler(webapp.RequestHandler):
 
 class TemplateListHandler(webapp.RequestHandler):
   def get(self):
-  #todo: protect with authentication
+    #todo: protect with authentication
     def readf(f):
       return {'name': f,
           'content': open(os.path.abspath(os.path.join('tpl', f))).read()}
@@ -91,25 +94,38 @@ class TemplateListHandler(webapp.RequestHandler):
 def readPage(page):
   if page is None:
     return page
+
+  name = desanitizePageName(page.key().name())
   return {
-      'name': desanitizePageName(page.key().name()),
+      'name': name,
+      'uri': 'http://%s/content-manager/pages/%s'% (os.environ.get('HTTP_HOST'), name),
       'rendered': page.rendered
       }
 
 class PageListHandler(webapp.RequestHandler):
   def get(self):
-  #todo: protect with authentication
+    #todo: protect with authentication
     self.response.headers['Content-Type'] = 'text/plain'
-
     self.response.out.write(
         simplejson.dumps([readPage(p) for p in Page.all()]))
 
 class PageHandler(webapp.RequestHandler):
+  def get(self, page_name):
+    #todo: protect with authentication
+    page = Page.get_by_key_name(sanitizePageName(page_name))
+    if page is None:
+      self.response.set_status(404)
+      self.response.out.write('<h1>Todo: Create a better "not found" page.</h1>')
+      return
+
+    self.response.out.write(page.rendered)
+
   def put(self, page_name):
-  #todo: protect with authentication
+    #todo: protect with authentication
     self.response.headers['Content-Type'] = 'text/plain'
+
     if re.search('[^\w]+', page_name):
-      self.response.set_status(409)
+      self.response.set_status(400)
       self.response.out.write('unallowed characters in page name')
       return
 
@@ -120,9 +136,92 @@ class PageHandler(webapp.RequestHandler):
       self.response.set_status(201)
       page = Page(key_name=sanitizePageName(page_name))
 
-    # todo: make updates
+    page_data = simplejson.loads(self.request.body)
+    if not page_data.get('template'):
+      self.response.set_status(400)
+      self.response.out.write('page data must include a template name')
+      return
+
+    try:
+      page.rendered = template.render(
+          os.path.join(
+            os.path.dirname(__file__), 'tpl', page_data.get('template')),
+          page_data.get('context')) 
+    except Exception, e:
+      self.response.set_status(400)
+      self.response.out.write(repr(e))
+      return
+
     page.put()
     self.response.out.write(simplejson.dumps(readPage(page)))
+
+def readContentItem(item):
+  if item is None:
+    return item
+  return {
+      'id': str(item.key()),
+      'content': item.content 
+      }
+
+class InventoryListHandler(webapp.RequestHandler):
+  def get(self):
+    #todo: protect with authentication
+    self.response.headers['Content-Type'] = 'text/plain'
+    #todo: as the content inventory gets large,
+    # a wholesale data dump might not be a good idea
+    self.response.out.write(
+        simplejson.dumps([readContentItem(i) for i in ContentItem.all()]))
+
+  def post(self):
+    #todo: protect with authentication
+    context = simplejson.loads(self.request.body)
+    id = context.get('id')
+    #todo: sanitize content ??
+    content = db.Text(context.get('content'))
+
+    item = None
+
+    if id:
+      k = None 
+      try:
+        k = db.Key(id)
+      except:
+        self.response.set_status(400)
+        self.response.out.write('invalid id: %s'% id)
+        return
+
+      item = db.get(k)
+
+      if item is None:
+        self.response.set_status(400)
+        self.response.out.write('id not found: %s'% id)
+        return
+
+      item.content.append(content)
+      item.put()
+
+    else:
+      item = ContentItem(content=[content])
+      item.put()
+        
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.set_status(200)
+    self.response.out.write(simplejson.dumps(readContentItem(item)))
+
+class EnvironsHandler(webapp.RequestHandler):
+  def get(self):
+    for name in os.environ.keys():
+      self.response.out.write("%s = %s<br />\n" % (name, os.environ[name]))
+
+  def post(self):
+    headers = self.request.headers
+    r = '' 
+    for header in headers:
+      r += header +" = "+ headers[header] +"\n"
+    b = self.request.body
+    self.response.headers["Content-Type"] = "text/plain"
+    self.response.out.write("\n << REQUEST HEADERS >>\n"+ r)
+    self.response.out.write("\n << REQUEST BODY >>\n"+ b)
 
 application = webapp.WSGIApplication([
   # Homepage
@@ -151,8 +250,14 @@ application = webapp.WSGIApplication([
   # Admin: view page entities
   ('/content-manager/pages/', PageListHandler),
 
-  # PUT a page entity (compiles templates)
+  # Admin: PUT or GET a page entity (PUT compiles templates)
   ('/content-manager/pages/(.*)', PageHandler),
+
+  # Admin: view or POST content entities
+  ('/content-manager/inventory/', InventoryListHandler),
+
+  # Admin: print out env variables
+  ('/environs', EnvironsHandler),
 
   # Not Found
   ('/.*', NotFoundHandler)
--- google-app-engine/tpl/index.tpl
+++ google-app-engine/tpl/index.tpl
@@ -1,35 +1,38 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Fireworks Project</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>{% block title %}The Fireworks Project{% endblock %}</title>
+  {% block style %}{% endblock %}
+  {% block head %}{% endblock %}
 </head>
 <body>
-	<div id="page">
-		<div id="header">
-			<h1 id="logo"><a href="#">Fireworks Project</a></h1>
-			<ul id="navigation">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Project</a></li>
-				<li><a href="#">Contribute</a></li>
-				<li><a href="#">About</a></li>
-			</ul>
-			<form id="search">
-				<div>
-					<input type="text" id="s" name="s" value="Search">
-					<button>Go</button>
-				</div>
-			</form>
-		</div>
-		<div id="masthead">
-			<!-- Featured Content Slider -->
-		</div>
-		<div id="content">
-			<!-- Content -->
-		</div>
-		<div id="footer">
-			<!-- Copyright, Site Map, Miscellaneous -->
-		</div>
-	</div>
+<div id="page">
+  <div id="header">
+    <h1 id="logo"><a href="/">Fireworks Project</a></h1>
+    <ul id="navigation">
+      <li><a href="/">{{nav.home}}</a></li>
+      <li><a href="/projects/">{{nav.projects}}</a></li>
+      <li><a href="/join">{{nav.join}}</a></li>
+      <li><a href="/about">{{nav.about}}</a></li>
+    </ul>
+  </div>
+
+  {# <!-- Featured Content Slider --> #}
+  <div id="masthead">
+    {% block masthead %}{% endblock %}
+  </div>
+
+  {# <!-- Content --> #}
+  <div id="content">
+    {% block content %}{% endblock %}
+  </div>
+
+  {# <!-- Copyright, Site Map, Miscellaneous --> #}
+  <div id="footer">
+    {% block footer %}{% endblock %}
+  </div>
+
+</div><!-- page -->
 </body>
 </html>
