d6270023b3475773b9324d25e7b4fd6328256a55
Kristoffer Walker <kixxauth@gmail.com>
2010-05-20 16:23:28 -0400

    Testing refactor.
     On branch version_5
    	modified:   test/config.yaml
    	deleted:    test/tests/basic.py
    	modified:   test/tests/test_utils.py
    	new file:   test/tests/urls.py

--- test/tests/test_utils.py
+++ test/tests/test_utils.py
@@ -1,5 +1,6 @@
 import re
 import httplib
+import unittest
 
 # The current host name we're testing on.
 HOST = None
@@ -8,27 +9,17 @@ HOST = None
 # False if not.
 LOCAL = True
 
+def setup(host, local):
+  """Global setup function called by testrunner.py"""
+  global HOST
+  global LOCAL
+
+  HOST = host
+  LOCAL = local
+
 # If you don't know what this is you need to read the HTTP spec.
 # http://tools.ietf.org/html/rfc2616#section-9
 METHODS = ['GET','POST','PUT','DELETE','OPTIONS','HEAD','TRACE']
-
-# This is a set of request headers that mimic a web browser.
-# Again, if you don't know what a header is you should read the HTTP spec.
-# http://tools.ietf.org/html/rfc2616#section-14
-#
-# TODO: Test other browsers. (This is Firefox 3.6.3)
-BROWSER_HEADERS = dict([
-    ('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3'),
-    ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
-    # TODO: Test other languages.
-    ('Accept-Language', 'en-us,en;q=0.5'),
-    ('Accept-Encoding', 'gzip,deflate'),
-    ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
-    ('Keep-Alive', '115'),
-    ('Connection', 'keep-alive'),
-    ('Pragma', 'no-cache'),
-    ('Cache-Control', 'no-cache')
-  ])
 
 # This is a regex for testing the correctness of an HTTP date field like
 # in a Date or Expiration header.
@@ -39,28 +30,7 @@ HTTP_DATE_RX = re.compile('^[SMTWF]{1}[unoedhriat]{2}, [0-3]{1}[0-9]{1} [JFMASON
 # it out all over the place.
 NO_CACHE_HEADER = 'no-cache, no-store, must-revalidate, pre-check=0, post-check=0'
 
-def update_browser_headers(new_headers=None):
-  """Make a copy of the standard browser headers dict and update it before
-  returning it.
-  """
-  headers = dict(BROWSER_HEADERS)
-  if isinstance(new_headers, list):
-    headers.update(new_headers)
-  return headers
-
-def make_fake_body(method=None):
-  """Create a return a string to make a fake body for a request.
-  
-  If `method` is given it should be one of the HTTP method names as an
-  uppercase string. If it is 'PUT' or 'POST' or None, a string is returned that
-  is suitable for use as a request with content type
-  `application/x-www-form-urlencoded`,
-  """
-  body = 'k=tfp&num=44'
-  if method != None:
-    return (method == 'POST' or method == 'PUT') and body or None
-
-def check_headers(test, response, headers, msg=''):
+def test_response_headers(test, response, headers, msg=''):
   """Iterate through a list of headers, testing each one.
 
   `test` Should be the test object.
@@ -70,17 +40,18 @@ def check_headers(test, response, headers, msg=''):
     where directive may be 'eq', 'regex', or 'len'.
   `msg` Should be a message string to append on the end of the failure message.
   """
-  for type, k, expected_value in headers:
+  for k, cmp, expected_value in headers:
     value = response.headers.get(k)
-    if type == 'regex':
+    if cmp == 'regex':
       assert expected_value.match(value), \
-          ('header %s: %s ; does not match regex %s; %s' %
+          ('header "%s: %s" does not match regex %s; in %s' %
               (k, value, expected_value, msg))
-      continue
-    if type == 'len':
-      value = len(value)
-    test.assertEqual(value, expected_value,
-        "header '%s' is %s; %s" % (k, value, msg))
+    elif cmp == 'len':
+      test.assertEqual(len(value), expected_value,
+          'header "%s: %s" length != %d; %s' % (k, value, expected_value, msg))
+    else:
+      test.assertEqual(value, expected_value,
+            'header "%s: %s" != %s; %s' % (k, value, expected_value, msg))
 
 class Response(object):
   """Object constructor utility used by make_http_request()
@@ -102,12 +73,120 @@ def make_http_request(method='POST', url='/', body=None, headers={}):
   cxn.close()
   return rv
 
-def setup(host, local):
-  """Global setup function called by testrunner.py"""
-  global HOST
-  global LOCAL
+class TestRequest(object):
+  """Utility object constructor to make creating a test request object easier.
+  """
+  def __init__(self, parent=None):
+    # If we're passed a parent object we copy it...
+    if parent:
+      self.headers = dict(parent.headers.items())
+      if self.headers.get('Host'):
+        self.headers['Host'] = HOST
+      self.body = parent.body
+      self.response_status = parent.response_status
+      self.response_headers = list(parent.response_headers)
+      self.response_body = parent.response_body
 
-  HOST = host
-  LOCAL = local
-  BROWSER_HEADERS.update([('Host', HOST)])
+    # otherwise we create a new one.
+    else:
+      self.headers = {}
+      self.body = ''
+      self.response_status = 200
+      self.response_headers = []
+      self.response_body = True
+
+class TestConfig(object):
+  """Utility object constructor used to make test configuration easier.
+  """
+  def __init__(self, parent=None):
+    if parent:
+      self.configs = dict(parent.items())
+    else:
+      self.configs = {}
+
+  def items(self):
+    return self.configs.items()
+
+  def update(self, item, request):
+    self.configs[item] = request
+    return self
+
+def do_test(test, name, method, request):
+  """Utility function used to make a request and test the response.
+  """
+  # Call the server.
+  response = make_http_request(
+      method=method,
+      url=test.url,
+      body=request.body,
+      headers=request.headers)
+
+  # Check the HTTP status.
+  test.assertEqual(response.status, request.response_status,
+      ('HTTP status %d is not %d in %s for method %s' %
+        (response.status,
+          request.response_status,
+          name,
+          method)))
+
+  # Check all the response headers.
+  test_response_headers(
+      test,
+      response,
+      request.response_headers,
+      'in %s for method %s' % (name, method))
+
+  # Check the response body depending on the expected type.
+
+  # None or False; we check to make sure there is no response body.
+  if not request.response_body:
+    assert response.body == '' or response.body is None, \
+        ('response body is expected to be None in %s for method %s'%
+          (name, method))
+
+  # In the case of an integer we test for length.
+  elif isinstance(request.response_body, int):
+    test.assertEqual(len(response.body), request.response_body,
+      ('response body length is %d but expected to be %d in %s for method %s' %
+        (len(response.body),
+          request.response_body,
+          name,
+          method)))
+
+  # In the case of True, we just make sure the body was returned.
+  elif request.response_body is True:
+    assert response.body, \
+      ('response body does not exist in %s for method %s' %
+        (name, method))
+
+  # Lastly we do a regex check.
+  else:
+    try:
+      assert request.response_body.match(response.body), \
+          ('response body did not match regex %s in %s for method %s' %
+            (request.response_body.pattern, name, method))
+    except AttributeError, e:
+      raise Exception(
+        '.response_body attribute was expected to be a regex in %s' % name)
+
+def test_function(test_method):
+  """Decorator used to make a method into a URL test function.
+  """
+  method = test_method.__name__.upper()
+  constructor = test_method
+
+  def test_method_replacement(test):
+    if not getattr(test, 'issetup', None):
+      test.configure()
+      test.issetup = True
+
+    requests = constructor(test)
+    assert isinstance(requests, list), \
+        '%s.%s() must return a list' % (test.__name__, method)
+
+    for name, request in requests:
+      do_test(test, name, method, request)
+
+  test_method_replacement.__doc__ = test_method.__doc__
+  return test_method_replacement
 
---
+++ test/tests/urls.py
@@ -0,0 +1,453 @@
+import re
+import unittest
+import test_utils
+
+# Reassign the test decorator function to something nicer.
+test_function = test_utils.test_function
+
+# Create the request object we'll use for all the tests.
+firefox36_config = test_utils.TestRequest()
+
+# This is a set of request headers that mimic a web browser.
+# Again, if you don't know what a header is you should read the HTTP spec.
+# http://tools.ietf.org/html/rfc2616#section-14
+#
+# TODO: Test other browsers. (This is Firefox 3.6.3)
+firefox36_config.headers = dict([
+    ('Host', True),
+    ('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3'),
+    ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+    # TODO: Test other languages.
+    ('Accept-Language', 'en-us,en;q=0.5'),
+    ('Accept-Encoding', 'gzip,deflate'),
+    ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
+    ('Keep-Alive', '115'),
+    ('Connection', 'keep-alive'),
+    ('Pragma', 'no-cache'),
+    ('Cache-Control', 'no-cache')
+  ])
+
+# Finish setting up the test request configs.
+firefox36_config.body = ''
+firefox36_config.response_status = 200
+firefox36_config.response_headers = []
+firefox36_config.response_body = True
+
+class RobotsTxt(unittest.TestCase):
+  url = '/robots.txt'
+
+  def configure(self):
+
+    self.firefox36 = test_utils.TestRequest(firefox36_config)
+
+    if test_utils.LOCAL:
+      self.firefox36.response_headers = [
+            ('etag', 'eq', None),
+            ('server', 'eq', 'Development/1.0'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'regex', test_utils.HTTP_DATE_RX),
+            ('cache-control', 'eq', 'public, max-age=1814400'),
+            ('content-encoding', 'eq', None),
+            ('content-length', 'eq', '23'),
+            ('content-type', 'eq', 'text/plain')
+          ]
+      self.firefox36.response_body = re.compile('User-agent: \*\\nAllow: \/\\n')
+
+    else:
+      self.firefox36.response_headers = [
+            ('etag', 'len', 8),
+            ('server', 'eq', 'Google Frontend'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'regex', test_utils.HTTP_DATE_RX),
+            ('cache-control', 'eq', 'public, max-age=1814400'),
+            ('content-encoding', 'eq', 'gzip'),
+            ('content-length', 'eq', '43'),
+            ('content-type', 'eq', 'text/plain')
+          ]
+      self.firefox36.response_body = 43
+
+  @test_function
+  def get(self):
+    """GET request for robots.txt
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def put(self):
+    """PUT request for robots.txt
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 'User-agent: *\nAllow: /\n'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'text/plain'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def post(self):
+    """POST request for robots.txt
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 's=foo&num=44'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def delete(self):
+    """DELETE request for robots.txt
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def head(self):
+    """HEAD request for robots.txt
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    # GAE dev and production static file servers both set the Content-Length
+    # header even when there is no content body returned.
+    if test_utils.LOCAL:
+      ff36.response_headers[6] = ('content-length', 'eq', '23')
+    else:
+      ff36.response_headers[6] = ('content-length', 'eq', '43')
+    ff36.response_body = None
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def options(self):
+    """OPTIONS request for robots.txt
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def trace(self):
+    """TRACE request for robots.txt
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+class Sitemap(unittest.TestCase):
+  url = '/sitemap.xml'
+
+  def configure(self):
+
+    self.firefox36 = test_utils.TestRequest(firefox36_config)
+    self.firefox36.response_body = 1225
+
+    if test_utils.LOCAL:
+      self.firefox36.response_headers = [
+            ('etag', 'eq', None),
+            ('server', 'eq', 'Development/1.0'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'regex', test_utils.HTTP_DATE_RX),
+            ('cache-control', 'eq', 'public, max-age=86400'),
+            ('content-encoding', 'eq', None),
+            ('content-length', 'eq', '1225'),
+            ('content-type', 'eq', 'application/xml')
+          ]
+
+    else:
+      self.firefox36.response_headers = [
+            ('etag', 'len', 8),
+            ('server', 'eq', 'Google Frontend'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'regex', test_utils.HTTP_DATE_RX),
+            ('cache-control', 'eq', 'public, max-age=86400'),
+            # GAE does static file servers do not gzip xml.
+            ('content-encoding', 'eq', None),
+            ('content-length', 'eq', '1225'),
+            ('content-type', 'eq', 'application/xml')
+          ]
+
+  @test_function
+  def get(self):
+    """GET request for sitemap.xml
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def put(self):
+    """PUT request for sitemap.xml
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 'User-agent: *\nAllow: /\n'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'text/plain'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def post(self):
+    """POST request for sitemap.xml
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 's=foo&num=44'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def delete(self):
+    """DELETE request for sitemap.xml
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def head(self):
+    """HEAD request for sitemap.xml
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    # GAE dev and production static file servers both set the Content-Length
+    # header even when there is no content body returned.
+    ff36.response_headers[6] = ('content-length', 'eq', '1225')
+    ff36.response_body = None
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def options(self):
+    """OPTIONS request for sitemap.xml
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def trace(self):
+    """TRACE request for sitemap.xml
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+class GoogleVerify(unittest.TestCase):
+  url = '/googlef734612d306d87e6.html'
+
+  def configure(self):
+
+    self.firefox36 = test_utils.TestRequest(firefox36_config)
+
+    if test_utils.LOCAL:
+      self.firefox36.response_headers = [
+            ('etag', 'eq', None),
+            ('server', 'eq', 'Development/1.0'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'regex', test_utils.HTTP_DATE_RX),
+            ('cache-control', 'eq', 'public, max-age=1814400'),
+            ('content-encoding', 'eq', None),
+            ('content-length', 'eq', '53'),
+            ('content-type', 'eq', 'text/html')
+          ]
+      self.firefox36.response_body = 53
+
+    else:
+      self.firefox36.response_headers = [
+            ('etag', 'len', 8),
+            ('server', 'eq', 'Google Frontend'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'regex', test_utils.HTTP_DATE_RX),
+            ('cache-control', 'eq', 'public, max-age=1814400'),
+            ('content-encoding', 'eq', 'gzip'),
+            ('content-length', 'eq', '70'),
+            ('content-type', 'eq', 'text/html')
+          ]
+      self.firefox36.response_body = 70
+
+  @test_function
+  def get(self):
+    """GET request for Google Verify
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def put(self):
+    """PUT request for Google Verify
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 'User-agent: *\nAllow: /\n'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'text/plain'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def post(self):
+    """POST request for Google Verify
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 's=foo&num=44'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def delete(self):
+    """DELETE request for Google Verify
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def head(self):
+    """HEAD request for Google Verify
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    # GAE dev and production static file servers both set the Content-Length
+    # header even when there is no content body returned.
+    if test_utils.LOCAL:
+      ff36.response_headers[6] = ('content-length', 'eq', '53')
+    else:
+      ff36.response_headers[6] = ('content-length', 'eq', '70')
+    ff36.response_body = None
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def options(self):
+    """OPTIONS request for Google Verify
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def trace(self):
+    """TRACE request for Google Verify
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+class NotFound(unittest.TestCase):
+  url = '/lost_city_of_atlantis'
+
+  def configure(self):
+
+    self.firefox36 = test_utils.TestRequest(firefox36_config)
+    self.firefox36.response_status = 404
+
+    if test_utils.LOCAL:
+      self.firefox36.response_headers = [
+            ('etag', 'eq', None),
+            ('server', 'eq', 'Development/1.0'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'eq', '-1'),
+            ('pragma', 'eq', 'no-cache'),
+            ('cache-control', 'eq', test_utils.NO_CACHE_HEADER),
+            ('content-encoding', 'eq', None),
+            ('content-length', 'eq', '238'),
+            ('content-type', 'eq', 'text/html; charset=utf-8'),
+            ('x-xss-protection', 'eq', '0')
+          ]
+      self.firefox36.response_body = 238
+
+    else:
+      self.firefox36.response_headers = [
+            ('etag', 'eq', None),
+            ('server', 'eq', 'Google Frontend'),
+            ('date', 'regex', test_utils.HTTP_DATE_RX),
+            ('expires', 'eq', '-1'),
+            ('pragma', 'eq', 'no-cache'),
+            ('cache-control', 'eq', test_utils.NO_CACHE_HEADER),
+            ('content-encoding', 'eq', 'gzip'),
+            ('content-length', 'eq', '200'),
+            ('content-type', 'eq', 'text/html; charset=utf-8'),
+            ('x-xss-protection', 'eq', '0')
+          ]
+      self.firefox36.response_body = 200
+
+  @test_function
+  def get(self):
+    """GET request for Not Found
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def put(self):
+    """PUT request for Not Found
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 'User-agent: *\nAllow: /\n'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'text/plain'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def post(self):
+    """POST request for Not Found
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.body = 's=foo&num=44'
+    ff36.headers['Content-Length'] = str(len(ff36.body))
+    ff36.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def delete(self):
+    """DELETE request for Not Found
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def head(self):
+    """HEAD request for Not Found
+    """
+    ff36 = test_utils.TestRequest(self.firefox36)
+    ff36.response_headers[6] = ('content-encoding', 'eq', None)
+    ff36.response_headers[7] = ('content-length', 'eq', '0')
+    ff36.response_body = None
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', ff36)
+    return configs.items()
+
+  @test_function
+  def options(self):
+    """OPTIONS request for Not Found
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
+  @test_function
+  def trace(self):
+    """TRACE request for Not Found
+    """
+    configs = test_utils.TestConfig()
+    configs.update('firefox36', self.firefox36)
+    return configs.items()
+
