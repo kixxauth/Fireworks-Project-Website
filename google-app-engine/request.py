275060e1b7907d7df401e1b1558ef7d766e4b1d1
Kristoffer Walker <kixxauth@gmail.com>
2010-05-25 10:57:06 -0400

    Prune unused store module.
     On branch version_5
    	modified:   google-app-engine/request.py
    	deleted:    google-app-engine/store.py

--- google-app-engine/request.py
+++ google-app-engine/request.py
@@ -4,9 +4,12 @@ from fwerks import App
 from google.appengine.ext.webapp.util import run_bare_wsgi_app
 from handlers import handler_map, exception_handler, not_found
 
+# Create a fwerks application object. Fwerks is our quick and dirty WSGI
+# framework built with Werkzeug.
 fireworks_project_website = App(handler_map, exception_handler, not_found)
 
 def main():
+  # Use GAE helper function to run the WSGI app.
   run_bare_wsgi_app(fireworks_project_website)
 
 if __name__ == "__main__":
